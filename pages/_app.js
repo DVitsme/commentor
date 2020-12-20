c992946d2e531116927ef75e9480ef8c4fea1665
dvalen <derrick.valentine@gmail.com>
2020-12-20 03:12:53 -0500

    set up absoulte paths

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,26 @@
-import { AuthProvider } from '../lib/auth';
 import { ChakraProvider } from '@chakra-ui/react';
 
-import theme from '../styles/theme';
+import { AuthProvider } from '@/lib/auth';
+import theme from '@/styles/theme';
+import { css, Global } from '@emotion/react';
 
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider theme={theme}>
       <AuthProvider>
+        <Global
+          styles={css`
+            html {
+              min-width: 360px;
+              scroll-behavior: smooth;
+            }
+            #__next {
+              display: flex;
+              flex-direction: column;
+              min-height: 100vh;
+            }
+          `}
+        />
         <Component {...pageProps} />
       </AuthProvider>
     </ChakraProvider>
