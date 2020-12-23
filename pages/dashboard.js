a13f788145cdd02545146a0e33030e03ed208673
dvalen <derrick.valentine@gmail.com>
2020-12-23 15:15:00 -0500

    Skeleton preloading

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -4,11 +4,21 @@ import { Text, Button, ButtonGroup, Icon, Flex } from '@chakra-ui/react';
 import { DiTerminal } from 'react-icons/di';
 import { Head } from 'next/head';
 import EmptyState from '@/components/EmptyState';
+import SiteTableSkeleton from '@/components/SiteTableSkeleton';
+import DashboardShell from '@/components/DashboardShell';
 
 export default function Dashboard() {
   const auth = useAuth();
   if (!auth.user) {
-    return 'Loading...';
+    return (
+      <DashboardShell>
+        <SiteTableSkeleton />
+      </DashboardShell>
+    );
   }
-  return <EmptyState />;
+  return (
+    <DashboardShell>
+      <EmptyState />;
+    </DashboardShell>
+  );
 }
