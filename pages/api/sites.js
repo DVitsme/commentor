672f363934da9eac81a548eeee1de11dedc2dcdb
dvalen <derrick.valentine@gmail.com>
2020-12-23 15:14:23 -0500

    Gonna set up some kool Firebase Admin stuff

---
+++ pages/api/sites.js
@@ -0,0 +1,23 @@
+import db from '@/lib/firebase-admin';
+
+export default async (req, res) => {
+  const snapshot = await db.collection('sites').get();
+  const sites = [];
+
+  console.log(snapshot);
+  snapshot.forEach((doc) => {
+    sites.push({ id: doc.id, ...doc.data() });
+  });
+  // const getDoc = sitesRef
+  //   .get()
+  //   .then((doc) => {
+  //     if (!doc.exists) {
+  //       console.log('No such document');
+  //     }
+  //     res.status(200).json(doc.data());
+  //   })
+  //   .catch((err) => {
+  //     console.log(`Error getting doc: ${err}`);
+  //   });
+  res.status(200).json(sites);
+};
